a701bb8519b9178ded4c571f23ea0ecdc0f1b9bd
Trisell <ddesimini@outlook.com>
2015-03-31 13:34:22 -0600

    added Save

--- PythonNotepad.py
+++ PythonNotepad.py
@@ -8,7 +8,7 @@ root = Tkinter.Tk(className="My Text Editor")
 root.configure(background='green')
 textPad = ScrolledText()
 textPad.pack(fill='both', expand=1)
-
+file = ''
 # Clears Text Area for new Document
 def filenew():
     if tkMessageBox.askokcancel(title="Clean Window", message="Do you want to clear your data?"):
@@ -16,21 +16,27 @@ def filenew():
 
 # Open File Menu Item
 def fileopen():
-    file = tkFileDialog.askopenfile(mode='rb',title="Select a file")
+    global file
+    file = tkFileDialog.askopenfile(mode='r',title="Select a file")
     if file != None:
         contents = file.read()
         textPad.insert('1.0',contents)
-        file.close()
+
 
 # Save As File Menu Item
-def filesaveas():
-    file = tkFileDialog.asksaveasfile(mode='w',title="Save As", defaultextension=".txt")
-    if file != None:
+def filesave():
+    savefile = tkFileDialog.asksaveasfile(mode='w',title="Save", defaultextension=".txt")
+    if savefile != None:
         data = str(textPad.get(1.0,END))
-        file.write(data)
-        file.close()
+        savefile.write(data)
 
+
+
+# Exit File Menu
+def exit():
+    root.destroy()
 # Menu Item Place Holder To Prove Functionality
+
 def dummy():
     print "Some stuff."
 
@@ -43,10 +49,9 @@ filemenu = Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
 filemenu.add_command(label="New", command=filenew)
 filemenu.add_command(label="Open", command=fileopen)
-filemenu.add_command(label="Save", command=dummy)
-filemenu.add_command(label="Save As", command=filesaveas)
+filemenu.add_command(label="Save", command=filesave)
 filemenu.add_separator()
-filemenu.add_command(label="Exit", command=dummy)
+filemenu.add_command(label="Exit", command=exit)
 helpmenu = Menu(menu)
 menu.add_cascade(label="Help", menu=helpmenu)
 helpmenu.add_command(label="About", command=dummy)
